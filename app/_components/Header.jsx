70d93be615e1752c155900034bb1e473f4fc12d7
Ritik Kumar <ritikraj2018634@gmail.com>
2024-05-19 23:40:56 +0530

    clerk auth added

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { LogoMobile } from './Logo'
-import { Button } from '@/components/ui/button'
+import React from "react";
+import { LogoMobile } from "./Logo";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+
 
 const Header = () => {
-  return (
-    <div className=' p-5 flex justify-between items-center border shadow-sm' >
-        <LogoMobile/>
-        <Button>Get started</Button>
-    </div>
-  )
-}
+ const user = currentUser();
 
-export default Header
+  return (
+    <div className=" p-5 flex justify-between items-center border shadow-sm">
+      <LogoMobile />
+
+      <SignedIn>
+        <UserButton  />
+      </SignedIn>
+
+      <SignedOut>
+        <Button >
+          <SignInButton/>
+        </Button>
+      </SignedOut>
+
+      
+    </div>
+  );
+};
+
+export default Header;
